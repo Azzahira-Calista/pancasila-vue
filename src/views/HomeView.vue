4563ae0f35338ba021cdc2167c3960b09839233d
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2023-10-11 15:11:54 +0700

    feat: [CA] add HomePage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello Vue</h1>
+    <p>Test Pages</p>
   </div>
 </template>
 
