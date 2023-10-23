ba54d22b3afadeeebde288e6f29bb40fc59f95a1
Fadza <fadza0077@gmail.com>
2023-10-23 23:31:18 +0700

    feat: [FA] add About View and About Component Emblem Area

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+    <AboutCompEmblemAreaVue/>
+    <FooterComponent/>
+</template>
+
+<script>
+import AboutCompEmblemAreaVue from '@/components/About Component/AboutCompEmblemArea/AboutCompEmblemArea.vue'
+import FooterComponent from '@/components/Global Component/FooterComponent.vue';
+
+export default {
+    components: {
+        AboutCompEmblemAreaVue,
+        FooterComponent
+    }
+}
+</script>
+
