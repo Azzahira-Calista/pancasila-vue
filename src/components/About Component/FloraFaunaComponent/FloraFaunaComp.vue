a9b2e977f26662b21805a37927576b7a1185428e
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2023-10-26 23:45:35 +0700

    feat: [CA add about component visi misi, style flora fauna

--- src/components/About Component/FloraFaunaComponent/FloraFaunaComp.vue
+++ src/components/About Component/FloraFaunaComponent/FloraFaunaComp.vue
@@ -8,16 +8,17 @@
     <div class="flex justify-center items-center">
       <img class="rotate-180 w-8 h-8 mr-28" :src="iconPath" :alt="alt" />
 
-      <div
-        class="card bg-slate-600 h-[31.8rem] w-[62.5rem] rounded-2xl relative"
-      >
-        <div class="flex flex-col ml-[2.1rem] justify-end h-full mb-[2.5rem]">
-          <h2
-            class="font-poppins font-bold text-4xl text-[#ffffff] "
-          >
-            Title
-          </h2>
-          <p class="">description</p>
+      <div class="card h-[31.8rem] w-[62.5rem] rounded-2xl relative">
+        <img :src="imagePath" class="absolute" />
+        <div
+          class="flex flex-col ml-[2.1rem] justify-end h-full mb-[2.5rem] absolute"
+        >
+          <h2 class="font-poppins font-bold text-4xl text-[#ffffff]">Kuskus</h2>
+          <p class="mb-9 font-inter text-base text-[#ffffff] w-96">
+            Kuskus adalah mamalia berkantung <i>(Marsupialia) </i> nokturnal
+            termasuk dalam famili Phalangeridae. Kelompok hewan ini
+            persebarannya terbatas di Indonesia bagian timur.
+          </p>
         </div>
       </div>
       <img class="w-8 h-8 ml-28" :src="iconPath" :alt="alt" />
@@ -30,6 +31,7 @@ export default {
   data() {
     return {
       iconPath: require("@/assets/icon/double_arrow_black.svg"),
+      imagePath: require("@/assets/images/fauna1.png"),
     };
   },
 };
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,24 +1,27 @@
 <template>
-    <AboutCompHerovue/>
-    <FloraFaunaCompVue/>
-    
-    <AboutCompEmblemAreaVue/>
-    <FooterComponent/>
+  <div class="bg-primary">
+    <AboutCompHerovue />
+    <FloraFaunaCompVue />
+    <AboutCompVisiVue />
+    <AboutCompEmblemAreaVue />
+    <FooterComponent />
+  </div>
 </template>
 
 <script>
-import AboutCompEmblemAreaVue from '@/components/About Component/AboutCompEmblemArea/AboutCompEmblemArea.vue'
-import FooterComponent from '@/components/Global Component/FooterComponent.vue';
-import FloraFaunaCompVue from '@/components/About Component/FloraFaunaComponent/FloraFaunaComp.vue';
-import AboutCompHerovue from '@/components/About Component/AboutCompHero/AboutCompHero.vue';
+import AboutCompEmblemAreaVue from "@/components/About Component/AboutCompEmblemArea/AboutCompEmblemArea.vue";
+import FooterComponent from "@/components/Global Component/FooterComponent.vue";
+import FloraFaunaCompVue from "@/components/About Component/FloraFaunaComponent/FloraFaunaComp.vue";
+import AboutCompHerovue from "@/components/About Component/AboutCompHero/AboutCompHero.vue";
+import AboutCompVisiVue from "@/components/About Component/AboutCompVisiMisi/AboutCompVisi.vue";
 
 export default {
-    components: {
-        AboutCompEmblemAreaVue,
-        FooterComponent,
-        FloraFaunaCompVue,
-        AboutCompHerovue
-    }
-}
+  components: {
+    AboutCompEmblemAreaVue,
+    FooterComponent,
+    FloraFaunaCompVue,
+    AboutCompHerovue,
+    AboutCompVisiVue,
+  },
+};
 </script>
-
