698b46a6d0740c65c41ac95cf5199c05b048312d
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2023-10-26 15:08:32 +0700

    style: [CA] styling navbar

--- src/components/Global Component/NavbarComponent.vue
+++ src/components/Global Component/NavbarComponent.vue
@@ -5,18 +5,20 @@
     <img src="@/assets/icon/logo_web.svg" alt="Logo Web" />
     <div class="flex justify-between w-[21rem]">
       <router-link to="/">
-        <h2 class="text-[#DBEDDA] font-bold font-prompt text-2xl">Home</h2>
+        <h2 class="text-[#DBEDDA] font-semibold font-prompt text-2xl">Home</h2>
       </router-link>
 
       <router-link to="/about">
-        <h2 class="text-[#454545] font-normal font-prompt text-2xl">Tentang</h2>
+        <h2 class="text-[#DBEDDA] font-semibold font-prompt text-2xl">
+          Tentang
+        </h2>
       </router-link>
 
       <router-link to="/">
-        <h2 class="text-[#454545] font-normal font-prompt text-2xl">Wisata</h2>
+        <h2 class="text-[#DBEDDA] font-semibold font-prompt text-2xl">
+          Wisata
+        </h2>
       </router-link>
-
-      
     </div>
   </div>
 </template>
