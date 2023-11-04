ecc933fe04c646b07ddcf0086487b1b271e3a7f0
Fadza <fadza0077@gmail.com>
2023-11-05 04:32:15 +0700

    resolve and feat:[FA] add about component contribution

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,34 @@
 <template>
-    <AboutCompEmblemAreaVue/>
-    <FooterComponent/>
+  <div class="bg-primary">
+    <AboutCompHerovue />
+    <AboutCompFloraFaunaVue />
+    <AboutCompVisiVue />
+    <AboutCompHistoryVue/>
+    <AboutCompContributionVue/>
+    <AboutCompEmblemAreaVue />
+    <FooterComponent />
+  </div>
 </template>
 
 <script>
-import AboutCompEmblemAreaVue from '@/components/About Component/AboutCompEmblemArea/AboutCompEmblemArea.vue'
-import FooterComponent from '@/components/Global Component/FooterComponent.vue';
+import AboutCompEmblemAreaVue from "@/components/About Component/AboutCompEmblemArea/AboutCompEmblemArea.vue";
+import FooterComponent from "@/components/Global Component/FooterComponent.vue";
+import AboutCompHerovue from "@/components/About Component/AboutCompHero/AboutCompHero.vue";
+import AboutCompVisiVue from "@/components/About Component/AboutCompVisiMisi/AboutCompVisi.vue";
+import AboutCompFloraFaunaVue from '@/components/About Component/AboutComponentFloraFauna/AboutCompFloraFauna.vue';
+import AboutCompHistoryVue from '@/components/About Component/AboutCompHistory/AboutCompHistory.vue';
+import AboutCompContributionVue from '@/components/About Component/AboutCompContribution/AboutCompContribution.vue';
 
 export default {
-    components: {
-        AboutCompEmblemAreaVue,
-        FooterComponent
-    }
-}
+  components: {
+    AboutCompEmblemAreaVue,
+    FooterComponent,
+    AboutCompFloraFaunaVue,
+    AboutCompHerovue,
+    AboutCompVisiVue,
+    AboutCompHistoryVue,
+    AboutCompContributionVue,
+  },
+};
 </script>
 
