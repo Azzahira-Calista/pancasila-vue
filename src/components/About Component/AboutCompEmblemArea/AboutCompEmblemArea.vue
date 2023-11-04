d508263c1df98e3eb918764b0d4157c01e6c1578
Fadza <fadza0077@gmail.com>
2023-11-05 04:56:12 +0700

    style: [FA] add style about component emblem area

--- src/components/About Component/AboutCompEmblemArea/AboutCompEmblemArea.vue
+++ src/components/About Component/AboutCompEmblemArea/AboutCompEmblemArea.vue
@@ -3,8 +3,8 @@
     <h1 class="font-poppins font-bold text-[#626262] text-4xl mb-14">{{ heading }}</h1>
     <div class="flex justify-between w-full  h-full">
         <img :src= "emblemImage" :alt= "heading">
-        <div class="bg-white w-[40.37rem] h-[18.75rem] rounded-xl">
-            <div class="flex flex-col">
+        <div class="bg-white w-[40.37rem] h-[18.75rem] rounded-xl py-5 pl-5">
+            <div class=" h-full flex flex-col justify-center">
                 <PointTextVue v-for="text in emblemData" :key="text.id"
                 :pointColor="text.pointColor" 
                 :pointText="text.pointText" 
@@ -31,31 +31,31 @@ export default {
                 {
                     id: 1,
                     pointText: 'Komodo',
-                    pointColor: '#D78934',
+                    pointColor: 'text-[#D78934]',
                     descriptionText: 'Melambangkan Kekayaan Alam Khas NTT'
                 },
                 {
                     id: 2,
                     pointText: 'Tombak',
-                    pointColor: '#D78934',
+                    pointColor: 'text-[#FAA74A]',
                     descriptionText: 'Melambangkan Keagungan dan Kejayaan'
                 },
                 {
                     id: 3,
                     pointText: 'Pohon Beringin',
-                    pointColor: '#FFFFFF',
+                    pointColor: 'text-[#FFF000]',
                     descriptionText: 'Melambangkan Persatuan Dan Kesatuan'
                 },
                 {
                     id: 4,
                     pointText: 'Bintang',
-                    pointColor: '#FED100',
+                    pointColor: 'text-[#FED100]',
                     descriptionText: 'Melambangkan Ketuhanan Yang Maha Esa'
                 },
                 {
                     id: 5,
                     pointText: 'Padi Dan Kapas',
-                    pointColor: '#00BB3E',
+                    pointColor: 'text-[#00BB3E]',
                     descriptionText: 'Melambangkan Kemakmuran'
                 }
             ]
