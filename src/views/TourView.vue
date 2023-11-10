5557744cdc07125f381e7f1d45854a412ebd7121
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2023-11-10 16:10:05 +0700

    feat: [CA] add tour hero component

--- src/views/TourView.vue
+++ src/views/TourView.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
+    <TourCompHeroVue/>
     <TourComponentCoulinerVue/>
+    <FooterComponentVue/>
+
   </div>
 </template>
 
 <script>
 import TourComponentCoulinerVue from '@/components/Tour Component/TourComponentCouliner/TourComponentCouliner.vue'
+import FooterComponentVue from '@/components/Global Component/FooterComponent.vue'
+import TourCompHeroVue from '@/components/Tour Component/TourCompHero/TourCompHero.vue'
+
+
 
 export default {
   components: {
-    TourComponentCoulinerVue
+    TourComponentCoulinerVue,
+    FooterComponentVue,
+    TourCompHeroVue
   }
 }
 </script>
