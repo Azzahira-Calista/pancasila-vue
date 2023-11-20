50f9e90c3af7959f3459c2c8392fd424c023d117
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2023-11-20 20:58:53 +0700

    style: [CA] styling wisata page

--- src/components/Tour Component/TourCompWisata/TourWisataCard.vue
+++ src/components/Tour Component/TourCompWisata/TourWisataCard.vue
@@ -1,39 +1,12 @@
-<!-- <template>
-  <div class="rounded-2xl h-72 relative flex flex-col">
-    <div class="bg-slate-700 absolute"></div> -->
-<!-- <img class="w-full h-[17.25rem] absolute" :src="imageCard" :alt="title"> -->
-<!-- <h1 class="text-black text-3xl font-poppins font-normal absolute">
-      {{ titleCard }}
-    </h1>
-  </div>
-</template> -->
-
-<!-- <script>
-export default {
-  props: {
-    // imageCard: {
-    //   type: String,
-    //   required: true,
-    // },
-
-    titleCard: {
-      type: String,
-      required: true,
-    },
-  },
-};
-</script> -->
-
 <template>
-  <div class="bg-black rounded-3xl w-[25rem] h-60 relative overflow-hidden">
+  <div class="bg-black rounded-3xl w-[25rem] h-60 relative overflow-hidden transition duration-300 transform hover:scale-110">
     <img class="w-full h-full object-cover rounded-3xl" :src="imageCard" :alt="title" />
     <div class="absolute bottom-0 left-0 right-0 p-5">
-      <p class="font-inter text-2xl font-semibold text-white">
-        {{ titleCard }}
-      </p>
+      <p class="font-inter text-2xl font-semibold text-white">{{ titleCard }}</p>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   props: {
@@ -41,7 +14,6 @@ export default {
       type: String,
       required: true,
     },
-
     titleCard: {
       type: String,
       required: true,
--- src/views/TourView.vue
+++ src/views/TourView.vue
@@ -1,33 +1,26 @@
 <template>
-  <div class=" bg-primary">
-    <TourCompHeroVue/>
-    <TourCompWisataVue/>
-    <TourComponentCoulinerVue/>
-    <FooterComponentVue/>
-
+  <div class="bg-primary">
+    <TourCompHeroVue />
+    <TourCompWisataVue />
+    <TourComponentCoulinerVue />
+    <FooterComponentVue />
   </div>
 </template>
 
 <script>
-
-import TourComponentCoulinerVue from '@/components/Tour Component/TourComponentCouliner/TourComponentCouliner.vue'
-import FooterComponentVue from '@/components/Global Component/FooterComponent.vue'
-import TourCompHeroVue from '@/components/Tour Component/TourCompHero/TourCompHero.vue'
-import TourCompWisataVue from '@/components/Tour Component/TourCompWisata/TourCompWisata.vue'
-
+import TourComponentCoulinerVue from "@/components/Tour Component/TourComponentCouliner/TourComponentCouliner.vue";
+import FooterComponentVue from "@/components/Global Component/FooterComponent.vue";
+import TourCompHeroVue from "@/components/Tour Component/TourCompHero/TourCompHero.vue";
+import TourCompWisataVue from "@/components/Tour Component/TourCompWisata/TourCompWisata.vue";
 
 export default {
   components: {
     TourComponentCoulinerVue,
-    FooterComponentVue,
     TourCompHeroVue,
-    TourCompWisataVue
-    FooterComponentVue
-
-  }
-}
+    TourCompWisataVue,
+    FooterComponentVue,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
